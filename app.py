28d510e7f0dd122209023d486322ed3d8f915b41
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-04-22 15:41:51 +0300

    commit python

---
+++ app.py
@@ -0,0 +1,237 @@
+# # Functions
+
+# from unittest import result
+
+# def greet(f_name ,l_name):
+#     print (f"Hi {f_name} {l_name}")
+#     print("welcome aboard")
+#     print("How old are you")
+
+# greet("Nancy", "Awuor")
+# greet("Joana","Munduru")
+# greet("Edrian","Keylan")
+
+# # perform a task
+
+
+# # return value
+# # Keyword Argument
+# def increament(number ,by):
+#     return number + by
+
+# print(increament(4 ,2))
+# print(increament (10, 20))
+
+# print("hello world")
+
+# # variables
+# age=20
+# name="Janie"
+# print("hello", name, "are you", age, "years old?")
+
+# name="John Smith"
+# age=30
+# is_new_patient= False
+# in_not_new_patient=True
+# print (is_new_patient)
+
+# # Receiving Inputs
+# name=input("what is your name")
+# print("hello" + name)
+
+# birth_year = input("Enter your birth_year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+# # Type conversation
+# birth_year = input("Enter your birth_year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+# int(20)
+# float(40.23)
+# bool(False)
+# str("Homecoming")
+
+# # Basic calculator program
+
+
+# first = input("First:")
+# first="20"
+# second = input("Second:")
+# second="15"
+
+# # convert str to int 
+# first = input("First:")
+# first="20"
+# second = input("Second:")
+# second="15"
+# sum= int(first) + int(second) 
+# print(sum)
+
+#  # float
+# first = input("First:")
+# second = input("Second:")
+# sum = float(first) + float(second) 
+# print(sum)
+
+# # Strings
+# course="Python for beginners"
+# print(course.upper())
+# print(course)
+# print(course.lower())
+# print(course.find('q'))
+# print(course.find('n'))
+# print(course.replace("for" ,"young"))
+# print("python" in  course)
+
+# # Arithmetic Operations
+# print(10+5)
+# print(10//3)
+# print(10/4)
+# print(10^3)
+# print(3**4)
+# print(course.title())
+# x=10
+
+# Augmented Asssignment operatoes
+# x=10
+# print(x*3)
+# x=x+3
+# print(x+3+3)
+# x+=3 
+# x=(10+3*2)
+# print(10/3)
+# Comparison Operators/Comparig values
+# from ast import IsNot
+
+
+# x=10
+# x=3==2
+# print(x*6)
+# x=3==6
+# x=3!=2
+# x>10
+
+# # Logical Operators
+# price=25
+# print(price>10 and price <30)
+# print(price <30)
+# print(price >30)
+# print(not price>10)
+# print(not price > 10)
+# print(price > 20 or 20)
+# print(30 and 20)
+# print(not price > 20)
+
+# # If Statements/Comparison operators
+# temperature=25
+# if temperature > 30:
+#     print("its a hot day")
+#     print("drink plenty of water")
+#     print("done")
+# elif  temperature > 20:
+#     print("its a nice day")
+# elif temperature > 10:
+#      print("its abit cold")
+# else: 
+#     print("its a cold day")
+#     print("done")
+
+# #  Exercise
+# Weight=170
+# #  (K)g or (L)bd: l //Pounds
+# # Weight in (Kg)= 76.5:
+# # Solution
+# Weight=input("weight:")
+# unit=input("(K)g or (L)bs:")
+# if unit.upper() == "K":
+#     converted = Weight/0.45
+#     print("weigt in Lbs:" + converted)
+# else:
+#     converted = Weight * 0.45
+#     print("Weight in Kgs: " + str(converted))
+
+
+    # variables iN Pyhton
+from socket import MSG_EOR
+
+
+name="John"
+faied_subjects="2"
+print("Dear Mrs.Badger")
+print("your son " + name + " is failing" + faied_subjects + " subjects")
+print(name + " will redo" + faied_subjects + " courses")
+name="Eric"
+print(name + " is doing well in geography.")
+
+print (type("hello"))
+print(type(1))
+print(type(1.64))
+print(type(True))
+a=int(1)
+b=int(2.5)
+c=int("3")
+d=float(1)
+e=float(2.1)
+f=bool(3.5)
+
+# Variables and data types
+item_name="bread"
+price=50.5
+stock=200
+is_in_stock=True
+print(item_name,price,stock)
+
+
+# Arithmetic operations
+a=11
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print("exponent:", a ** b)
+
+# Strings
+msg="welcome to Akirachix"
+print(msg)
+print(msg * 2)
+print(msg,msg)
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.title())
+print(len(msg))
+print(msg.count('x'))
+
+# Slicing Strings
+msg="welcome to python"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
